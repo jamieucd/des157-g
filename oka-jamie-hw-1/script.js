27957368d23e55f6131acfed6b76190f79050a8b
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-01-22 23:14:20 -0800

    process files
    added survey and analysis

---
+++ oka-jamie-hw-1/script.js
@@ -0,0 +1,11 @@
+console.log('reading js')
+
+// declare variable
+var tri = "";
+
+// declare loop
+// i starts at 0, i wont loop greater than 7 times, increase by increments of one
+for (var i=0; i < 7; i++) {
+  tri += ('#');
+  console.log(tri);
+}
