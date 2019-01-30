13c0ec8fed7300101c28a30815b591b28c9844a1
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-01-30 09:10:25 -0800

    adjusted css
    size and margin-top

--- studio1/script.js
+++ studio1/script.js
@@ -14,9 +14,15 @@ function processForm(evt){
 
     var myMsg=document.querySelector('#myMsg');
 
-    myMsg.innerHTML= 'Congratulation on the release of your movie '+name+'! According to New York Times review, it is one of the hottest '+genre+' movies of the year! Starring '+lead +' as the lead character in this '+adj+' movie that will leave audiences feeling '+emo+'! Produced, written, choreographed, and scored by '+favArtist+'.';
-msgSection.className='show';
+    console.log(genre + ':genre')
 
+    if (genre=='horror'){
+      myMsg.innerHTML= 'booo!';
+    } else {
+      myMsg.innerHTML= 'Congratulation on the release of your movie '+name+'! According to New York Times review, it is one of the hottest '+genre+' movies of the year! Starring '+lead +' as the lead character in this '+adj+' movie that will leave audiences feeling '+emo+'! Produced, written, choreographed, and scored by '+favArtist+'.';
+    }
+
+msgSection.className='show';
     evt.preventDefault();
     return false;
   }
