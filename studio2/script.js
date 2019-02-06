2e4dfc8dedc5a34bae6aacba495d3bd5ba816415
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-02-06 01:20:13 -0800

    html css
    studio 2 css and html

--- studio2/script.js
+++ studio2/script.js
@@ -1,1 +1,8 @@
 console.log('script.js');
+
+('use strict');
+
+var flask=document.querySelector('#flask');
+var open=document.querySelector('#openbook');
+var hilight=document.querySelector('#hilight');
+var hydro=document.querySelector('#hydro');
