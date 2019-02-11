5a0d363f62e7904ed6225fbcc013103f4d7d13ea
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-02-10 20:43:28 -0800

    css html js
    organized, fixed java, css

--- studio2/script.js
+++ studio2/script.js
@@ -2,7 +2,49 @@ console.log('script.js');
 
 ('use strict');
 
-var flask=document.querySelector('#flask');
-var open=document.querySelector('#openbook');
-var hilight=document.querySelector('#hilight');
-var hydro=document.querySelector('#hydro');
+var flask = document.querySelector('#flask');
+var openbook = document.querySelector('#openbook');
+var hilight = document.querySelector('#hilight');
+var book = document.querySelector('#book');
+
+var overopen = document.querySelector('#overopen');
+var overflask = document.querySelector('#overflask');
+var overhilight = document.querySelector('#overflask');
+var overbook = document.querySelector('#overflask');
+
+var closeopen = document.querySelector('#closeopen');
+var closeflask = document.querySelector('#closeflask');
+var closehilight = document.querySelector('#closehilight');
+var closebook = document.querySelector('#closebook');
+
+
+// open book
+openbook.addEventListener('click', function() {
+  overopen.style.display = 'block';
+}, false);
+closeopen.addEventListener('click', function() {
+  overopen.style.display = 'none';
+}, false);
+//
+// //hydroflask
+flask.addEventListener('click', function() {
+  overflask.style.display = 'block';
+}, false);
+closeflask.addEventListener('click', function() {
+  overflask.style.display = 'none';
+}, false);
+// //
+// Highlighter
+hilight.addEventListener('click', function() {
+  overhilight.style.display = 'block';
+}, false);
+closehilight.addEventListener('click', function() {
+  overhilight.style.display = 'none';
+}, false);
+//
+book.addEventListener('click', function() {
+  overbook.style.display = 'block';
+}, false);
+closebook.addEventListener('click', function() {
+  overbook.style.display = 'none';
+}, false);
