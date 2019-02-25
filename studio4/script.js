f23dddad925f7c970a953b0c2d5fa71ce685a40c
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-02-24 21:08:00 -0800

    script
    event functions

--- studio4/script.js
+++ studio4/script.js
@@ -3,17 +3,23 @@ console.log('reading js')
 var zenItem = 0;
 var zenBox = document.querySelector('#sandbox');
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
 
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+  if (zenItems>=5){
+    // resetAll.style.display='block';
+
+    return;
+  }
+
+  zenBox.appendChild(document.getElementById(data));
+  zenItems++;
 }
