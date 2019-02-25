f4836b1aaaf4b089498666a75af9285cbf4de5f7
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-02-24 23:32:58 -0800

    js

--- studio4/script.js
+++ studio4/script.js
@@ -3,7 +3,6 @@ console.log('reading js')
 var zenItem = 0;
 var zenBox = document.querySelector('#sandbox');
 
-var obj, x, y, prev_x, prev_y;
 
 // var resetAll=document.querySelector('#resetAll');
 
