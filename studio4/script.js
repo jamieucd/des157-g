93ef50b89ac5fd6747f69dcee4cf32d31efa017d
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-02-24 23:22:25 -0800

    Update script.js

--- studio4/script.js
+++ studio4/script.js
@@ -13,40 +13,17 @@ function allowDrop(ev) {
 
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
-
-  prev_x = x - obj.offsetLeft;
-   prev_y = y - obj.offsetTop;
 }
 
 function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   if (zenItem>=5){
-    obj.style.background = '#sandbox';
-    obj = false;
     // resetAll.style.display='block';
 
     return;
   }
 
-
-    if(obj) {
-     obj.style.left = (x - prev_x) + 'px';
-     obj.style.top = (y - prev_y) + 'px';
-   }
-  }
-
-
   zenBox.appendChild(document.getElementById(data));
   zenItem++;
 }
-
-
-
-document.getElementById('rock1').onmousedown = drag;
-document.getElementById('rock2').onmousedown = drag;
-document.getElementById('tree').onmousedown = drag;
-document.getElementById('bush').onmousedown = drag;
-document.getElementById('pagoda').onmousedown = drag;
-document.onmousemove = move;
-document.onmouseup = drop;
