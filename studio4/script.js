dd14c85c572ebd506ba04ebf5c8cc7a83fce4349
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-02-24 22:34:20 -0800

    js try
    seeing if this shit works

--- studio4/script.js
+++ studio4/script.js
@@ -1,28 +1,28 @@
 console.log('reading js')
 
-var zenItem = 0;
-var zenBox = document.querySelector('#sandbox');
-
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  if (zenItem>=6){
-    // resetAll.style.display='block';
-
-    return;
-  }
-
-  zenBox.appendChild(document.getElementById(data));
-  zenItem++;
-}
+// var zenItem = 0;
+// var zenBox = document.querySelector('#sandbox');
+//
+// function allowDrop(ev) {
+//   ev.preventDefault();
+// }
+//
+// function drag(ev) {
+//   ev.dataTransfer.setData("text", ev.target.id);
+// }
+//
+// function drop(ev) {
+//   ev.preventDefault();
+//   var data = ev.dataTransfer.getData("text");
+//   if (zenItem>=6){
+//     // resetAll.style.display='block';
+//
+//     return;
+//   }
+//
+//   zenBox.appendChild(document.getElementById(data));
+//   zenItem++;
+// }
 
 
 var dragItem = document.quearySelector('#rock1');
