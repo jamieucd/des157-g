cc97dd4e0cd303332b9c305776a00c1029a05b8e
JAMIE OKA <jamieoka@JAMIEs-MacBook-Pro-5.local>
2019-02-24 23:14:36 -0800

    Update script.js

--- studio4/script.js
+++ studio4/script.js
@@ -3,30 +3,30 @@ console.log('reading js')
 var zenItem = 0;
 var zenBox = document.querySelector('#sandbox');
 
-var obj, x, y, prev_x, prev_y;
-
-// var resetAll=document.querySelector('#resetAll');
-
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  if (zenItem>=5){
-    // resetAll.style.display='block';
-
-    return;
-  }
-
-  zenBox.appendChild(document.getElementById(data));
-  zenItem++;
-}
+// var obj, x, y, prev_x, prev_y;
+//
+// // var resetAll=document.querySelector('#resetAll');
+//
+// function allowDrop(ev) {
+//   ev.preventDefault();
+// }
+//
+// function drag(ev) {
+//   ev.dataTransfer.setData("text", ev.target.id);
+// }
+//
+// function drop(ev) {
+//   ev.preventDefault();
+//   var data = ev.dataTransfer.getData("text");
+//   if (zenItem>=5){
+//     // resetAll.style.display='block';
+//
+//     return;
+//   }
+//
+//   zenBox.appendChild(document.getElementById(data));
+//   zenItem++;
+// }
 
 function drag(e) {
     obj = e.target;
